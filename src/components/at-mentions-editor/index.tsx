eb71100d5aa4054879ff476881b914a29ac8fcb4
hugangjun <hugangjun@xiaomi.com>
2023-11-24 16:55:47 +0800

    fix: 样式调整

--- src/components/at-mentions-editor/index.tsx
+++ src/components/at-mentions-editor/index.tsx
@@ -67,7 +67,10 @@ export const AtMentionsEditor = (props: IProps) => {
         const btn = document.createElement('button')
         btn.dataset.person = JSON.stringify(item.data)
         btn.textContent = `@${item.data.userName}`
-        btn.setAttribute('style', 'color:#4387f4;border:none;background:transparent;padding:0;')
+        btn.setAttribute(
+          'style',
+          'color:#4387f4;border:none;background:transparent;padding:0;font-size:inherit',
+        )
         btn.contentEditable = 'false'
         btn.addEventListener(
           'click',
@@ -282,7 +285,10 @@ export const AtMentionsEditor = (props: IProps) => {
         const btn = document.createElement('button')
         btn.dataset.person = JSON.stringify(personItem)
         btn.textContent = `@${personItem.userName}`
-        btn.setAttribute('style', 'color:#4387f4;border:none;background:transparent;padding:0;')
+        btn.setAttribute(
+          'style',
+          'color:#4387f4;border:none;background:transparent;padding:0;font-size:inherit',
+        )
         btn.contentEditable = 'false'
         btn.addEventListener(
           'click',

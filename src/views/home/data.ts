6721728c7b700d927a1df9ba16eeb5d7f24395a4
hugangjun <hugangjun@xiaomi.com>
2024-02-02 16:51:57 +0800

    feat: 完善功能

---
+++ src/views/home/data.ts
@@ -0,0 +1,11 @@
+export const initData =
+  "# 一级标题\n朝辞白帝彩云间，千里江陵一日还。\n两岸猿声啼不住，轻舟已过万重山。\n***\n## 二级标题\n**加粗** *斜体* ~~删除线~~ ***加粗斜体***  `code` [百度](https://www.baidu.com/) \n***\n### 三级标题\n1. 一级列表\n   * 二级列表\n   * 二级列表\n2. 一级列表\n***\n#### 四级标题\n[x] 这是一个任务A [g]\n[ ] 这是一个任务B [r] [y]\n***\n#####五/六级标题\n> 这是引用语法\n>>这是引用语法\n>> * 这是引用语法@张三 \n***\n```javascript\n$(document).ready(function () {\n alert('JavaScript');\n});\n```\n| 左对齐 | 居中对齐 | 右对齐 |\n| :-----| :----: | ----: |\n| 单元格 | 单元格 | 单元格 |\n| 单元格 | 单元格 | 单元格 |"
+export const initMentions = [
+  {
+    userId: 'zhangsan',
+    userName: '张三',
+    avatar: 'https://api.multiavatar.com/zhangsan.png',
+    length: 3,
+    offset: 266,
+  },
+]
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,15 +3,16 @@ import { AtMentionsEditor } from '@/components/at-mentions-editor'
 import { IMention } from '@/components/at-mentions-editor/types'
 import './index.scss'
 import MarkdownPreview from '@/components/markdown-preview'
+import { initData, initMentions } from './data'
 
 function Home() {
-  const [pureString, setPureString] = useState('')
-  const [mentionList, setMentionList] = useState<IMention[]>([])
+  const [pureString, setPureString] = useState(initData)
+  const [mentionList, setMentionList] = useState<IMention[]>(initMentions)
   return (
     <div>
       <div className='home'>
         {/* @编辑器部分 */}
-        <div>
+        <div style={{ flex: 1 }}>
           <div className='home__title'>@编辑器：</div>
           <div className='home__tip'>「输入@字符弹出人员搜索选择」</div>
           <AtMentionsEditor
@@ -19,6 +20,7 @@ function Home() {
             value={pureString}
             mentions={mentionList}
             onChange={(value, mentions) => {
+              console.log('test', JSON.stringify(value), mentions)
               setPureString(value)
               setMentionList(mentions)
             }}
@@ -27,8 +29,8 @@ function Home() {
           />
         </div>
         {/* 结果展示部分 */}
-        <div style={{ marginLeft: '35px' }}>
-          <div className='home__title'>文本展示：</div>
+        <div style={{ marginLeft: '50px', flex: 1 }}>
+          <div className='home__title'>Markdown渲染展示：</div>
           <div className='home__tip'>「点击@人员展示个人卡片」</div>
           <MarkdownPreview mentions={mentionList}>{pureString}</MarkdownPreview>
         </div>
